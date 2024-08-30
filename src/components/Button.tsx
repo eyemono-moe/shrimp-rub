065447c4419379da0d993178cb8615b6c853e4ca
eyemono-moe <ysdr83@gmail.com>
2024-08-30 10:41:27 +0900

    feat: fix style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ const Button: Component<
   return (
     <button
       {...defaultProps}
-      class="rounded-full px-4 py-2 font-bold text-nowrap flex gap-1"
+      class="rounded-full px-4 py-2 font-bold text-nowrap inline-flex gap-1 items-center"
       classList={{
         "bg-blue-500 hover:bg-blue-600 text-white":
           defaultProps.variant === "primary",
