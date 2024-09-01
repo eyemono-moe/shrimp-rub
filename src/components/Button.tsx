83903cf854fd3c4dc9e769f7f2788d63c4fbab8d
eyemono-moe <ysdr83@gmail.com>
2024-09-01 23:52:29 +0900

    feat: add touch-action: manipulation  on buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ const Button: Component<
   return (
     <button
       {...defaultProps}
-      class="rounded-full px-4 py-2 font-bold text-nowrap inline-flex gap-1 items-center"
+      class="rounded-full px-4 py-2 font-bold text-nowrap inline-flex gap-1 items-center touch-manipulation"
       classList={{
         "bg-blue-500 hover:bg-blue-600 text-white":
           defaultProps.variant === "primary",
